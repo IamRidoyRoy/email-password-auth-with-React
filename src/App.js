4f2dc2b031f18e135740e3d42290fb22f8f68faf
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-04-12 15:15:23 +0600

    Setup a bootstrap form and prevent reload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { getAuth } from "firebase/auth";
 import './App.css';
 import app from "./firebase.init";
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const auth = getAuth(app);
 
 function App() {
@@ -11,15 +15,30 @@ function App() {
     console.log(event.target.value);
   }
   const handleOnSubmit = (event) => {
-    console.log(event.target.value);
+    console.log('Form Submitted');
+    event.preventDefault();
   }
   return (
-    <div className="App">
-      <form onSubmit={handleOnSubmit}>
-        <input onBlur={handleEmailBlur} type="text" /> <br />
-        <input onBlur={handlePassBlur} type="password" name="" id="" /><br />
-        <input type="submit" value="Login" />
-      </form>
+    <div className="registration w-50 mx-auto mt-5">
+      <h1 className="text-primary">Please Register!</h1>
+      <Form onSubmit={handleOnSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control onBlur={handleEmailBlur} type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control onBlur={handlePassBlur} type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
