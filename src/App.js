78da639748913de9ffd58945837cf9429a7eb79b
Ridoy Roy <ridoyroycse53engg@gmail.com>
2022-04-12 15:33:10 +0600

    React bootstrap registration form setup

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import app from "./firebase.init";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import { useState } from 'react'
 const auth = getAuth(app);
 
 function App() {
+
+  const [email, setEmail] = useState()
+
   const handleEmailBlur = event => {
     console.log(event.target.value);
   }
